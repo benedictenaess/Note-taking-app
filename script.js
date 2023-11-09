4e8870e2768be7d093a8887824927c819b1d2115
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2023-11-09 11:26:11 +0100

    added else statement

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const button = document.querySelector('.submit-button');
 const input = document.querySelector('.notes-text-input');
-const error = document.querySelector('.notes-error');
+const error = document.querySelector('.notes-error-text');
+const list = document.querySelector('.notes-list');
 
 button.addEventListener('click', (e)=>{
 	e.preventDefault()
+
+	
 	if (input.value === ''){
-		setInterval(() => {
-			error.style.display = 'block';
-		}, 0, 1000);
-	}
-	const listContainer = document.createElement('ul');
-	const listItems = document.createElement('li');
+		error.style.display = 'block';
+		setTimeout(() => {
+			error.style.display = 'none';
+		}, 1000);
+	} else {
+		const listContainer = document.createElement('ul');
+		const listItems = document.createElement('li');
+		list.append(listContainer);
+		listContainer.append(listItems);
+		listItems.textContent = input.value;
+		input.value = '';
+	}	
 })
